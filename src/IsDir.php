d6ff46ab15b3d866984639b9ce2bfbb61d5ef451
大菠萝 <@qq.com>
2023-10-04 08:54:49 +0800

    更新目录结构

--- src/IsDir.php
+++ src/IsDir.php
@@ -9,18 +9,22 @@ class IsDir
 
     public function __construct($childPath)
     {
-        $this->path = getcwd() . '\\';
-        if(strstr($this->path,'public') === false){
-            $this->childPath = 'public/'.$childPath;
-        }
-        $this->isdir($childPath);
-    }
-
-    protected function isdir($childPath)
-    {
-        if(!$childPath){
+        if (!$childPath) {
             throw new \Exception('请传入指定目录');
         }
+
+        $this->path = getcwd() . '\\';
+        if (strstr($this->path, 'public') === false) {
+            $this->childPath = 'public/' . $childPath;
+        } else {
+            $this->childPath = '\\' . $childPath;
+        }
+        $this->isdir();
+    }
+
+    protected function isdir()
+    {
+
         //循环检测目录是否存在，没有则创建
         $pathArr = explode('/', $this->childPath);
 
@@ -28,7 +32,7 @@ class IsDir
             if (!$ml) continue;
             $this->path .= $ml . '\\';
 
-            if (!is_dir($this->path )) {
+            if (!is_dir($this->path)) {
                 mkdir($this->path);
             }
         }
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'mydom/uploads',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7f511170dcb6de67f3882bac96818c8bf61281d2',
         'type' => 'dev',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'mydom/uploads' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7f511170dcb6de67f3882bac96818c8bf61281d2',
             'type' => 'dev',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
